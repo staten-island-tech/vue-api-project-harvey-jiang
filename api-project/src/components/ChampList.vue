9119d533911cf704d69466a097fe7e76b987478b
harveyj123 <harvey.jiang23@sitechhs.com>
2021-11-08 12:28:36 -0500

    as

--- api-project/src/components/ChampList.vue
+++ api-project/src/components/ChampList.vue
@@ -1,46 +1,18 @@
 <template>
-  <div>
-    <h1>TEST</h1>
-    <ul class="champ-table">
-      <li
-        class="champ-list-item"
-        v-for="champ in champions"
-        :key="champ.name"
-      >
-        {{ champ.name }}
-      </li>
-    </ul>
+  <div class="container">
+    <h1>{{ pokemon.name }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "PkmnList",
-  data() {
-    return { champions: [] ,
-    };
-  },
-  props: [],
-  mounted: function() {
-    this.fetchData();
-  },
-  methods: {
-    fetchData: async function() {
-      try {
-        const result = await fetch(
-          `http://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion.json?api_key=RGAPI-360739c5-1a96-46f6-b942-bcf95baa916c`
-        );
-        const data = await result.json();
-        console.log(data);
-        this.champions = data.results;
-
-      } catch (error) {
-        alert(error);
-      }
-    },
-  },
+  name: "ChampList",
+  props: ["pokemon"],
 };
 </script>
+
+<style>
+</style>
 
 <style>
 
--- api-project/src/views/Home.vue
+++ api-project/src/views/Home.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="home">
-    <ChampList></ChampList>
+    <ChampList
+      v-for="pokemon in pokemons"
+      :key="pokemon"
+      :pokemon="pokemon"
+    ></ChampList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import ChampList from "../components/ChampList.vue";
+import ChampList from "@/components/ChampList.vue";
 export default {
+  //options API
   name: "Home",
   components: {
     ChampList,
+  },
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created: function () {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function () {
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+        );
+        const data = await response.json();
+        this.pokemons = data.results;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
