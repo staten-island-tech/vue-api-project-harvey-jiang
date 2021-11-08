ab96f4d743b78db4163d34af966d63f2556edc22
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-11-08 00:07:37 -0500

    1

---
+++ api-project/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <ChampList></ChampList>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ChampList from "../components/ChampList.vue";
+export default {
+  name: "Home",
+  components: {
+    ChampList,
+  },
+};
+</script>
