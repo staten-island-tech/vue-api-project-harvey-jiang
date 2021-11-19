5f2e59388265db870b23f478e16e4ea4558286b2
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-11-18 19:23:55 -0500

    123

--- api-project/src/views/Champion.vue
+++ api-project/src/views/Champion.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-      <h1 class="test" 
+  <div class="about-champ-container">
+      <div class="about-champ" 
       v-for="champion in singleChampion"
       :key="champion.id">
+        <h1>
           {{champion.name}}
+        </h1>
           <p>
           {{champion.blurb}}
           </p>
-      </h1>
+      </div>
+      <figure class="champion-img-container">
+          <img class="img" :src="championImg"  alt="">
+      </figure>
   </div>
 </template>
 
@@ -16,6 +21,7 @@ export default {
     data() {
     return {
         singleChampion: [],
+        imgOne: [],
     }
     
 },
@@ -25,22 +31,55 @@ export default {
     methods: {
         fetchData: async function() {
           try {
+              let newData = [];
               const response = await fetch (
                   `http://ddragon.leagueoflegends.com/cdn/11.23.1/data/en_US/champion/${this.$route.params.id}.json`
               )
               const data = await response.json();
-              console.log( Object.keys(data.data))
               this.singleChampion = data.data
+              this.champImg = Object.keys(data.data)
+              this.imgOne = newData
+              
+              this.champImg.forEach((value) => {
+                newData.push(value);
+                console.log(value);
+              });
+
+              
+      
           } catch (error) {
               alert(error)
           }
       }
-    }
+    },
+        computed: {
+    championImg: function() {
+      return `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${this.imgOne}_0.jpg`;
+    },
+  }
+    
 }
 </script>
 
 <style>
-.test {
-    font-size: 2rem;
+
+.about-champ-container {
+    display: flex;
+height: 100vh;
+width: 100vw;
+}
+.about-champ {
+    height: 100%;
+    width: 50%;
+    font-size: 1.5rem;
+}
+
+.champion-img-container {
+    height: 100%;
+    width: 50%;
+}
+
+.img {
+    height: 50%;
 }
 </style>
